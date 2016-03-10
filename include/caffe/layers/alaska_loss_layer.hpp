99e976f4f0c6df0b87482275602eba9f3d5c0eef
kunhuang <kunhuang110@126.com>
2016-03-09 16:21:29 -0800

    bug fixed

--- include/caffe/layers/alaska_loss_layer.hpp
+++ include/caffe/layers/alaska_loss_layer.hpp
@@ -123,7 +123,7 @@ class AlaskaLossLayer : public LossLayer<Dtype> {
   /// How to normalize the output loss.
   LossParameter_NormalizationMode normalization_;
 
-  int softmax_axis_, outer_num_, inner_num_;
+  int softmax_axis_, outer_num_, inner_num_, label_num_;
 };
 
 }  // namespace caffe
